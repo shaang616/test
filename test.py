ff4bb9ffb7555141c385c61686a172e4c58d0e3c
root <you@example.com>
2020-06-15 15:58:05 +0530

    seventh commit

--- test.py
+++ test.py
@@ -1,2 +1,24 @@
-a=15
+a=16
 print(a);
+
+
+
+# https://zoom.us/j/93500977746?pwd=OStjUmQvQzBqRGtqVDREV0JDS1Vzdz09
+
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Sid": "PublicRead",
+#             "Effect": "Allow",
+#             "Principal": "*",
+#             "Action": "s3:GetObject",
+#             "Resource": "arn:aws:s3:::planetshare/*"
+#         }
+#     ]
+# }
+
+
+# accesskey: AKIA43SEOEU22B2R4VGX
+
+# secret:  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
